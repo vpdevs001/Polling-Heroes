69a92e7d16117f519d4abbf539f1a724e8a83be0
Ved Pandey <vpdevs001@gmail.com>
2026-05-16 10:19:07 +0530

    Fix Deployment Issues

--- backend/src/common/utils/cookieOptions.ts
+++ backend/src/common/utils/cookieOptions.ts
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === "production";
 export const accessTokenCookieOptions: CookieOptions = {
   httpOnly: true,
   secure: isProd,
-  sameSite: "lax",
+  sameSite: isProd ? "none" : "lax",
   maxAge: 7 * 24 * 60 * 60 * 1000,
   path: "/",
 };
