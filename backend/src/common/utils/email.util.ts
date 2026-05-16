4a0d55b7c57d40910fbeff90eb0f07647990e893
Ved Pandey <vpdevs001@gmail.com>
2026-05-16 12:33:17 +0530

    Fix mailing issue

--- backend/src/common/utils/email.util.ts
+++ backend/src/common/utils/email.util.ts
@@ -1,11 +1,12 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST || "smtp.mailtrap.io",
-  port: parseInt(process.env.SMTP_PORT || "2525"),
+  host: process.env.EMAIL_HOST,
+  port: Number(process.env.EMAIL_PORT),
+  secure: process.env.EMAIL_SECURE === "true",
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
