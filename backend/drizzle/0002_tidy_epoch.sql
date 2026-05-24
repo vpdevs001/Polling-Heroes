a5c2f5a49b30b81e34df01aca3ac9bc7db08294c
Ved Pandey <vpdevs001@gmail.com>
2026-05-24 23:32:32 +0530

    Build Polling Heroes

---
+++ backend/drizzle/0002_tidy_epoch.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "users" DROP COLUMN "is_verified";--> statement-breakpoint
+ALTER TABLE "users" DROP COLUMN "verification_token";--> statement-breakpoint
+ALTER TABLE "users" DROP COLUMN "verification_token_expires_at";
